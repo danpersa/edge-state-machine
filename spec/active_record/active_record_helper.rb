7c9c2db4606bfbc811d2908937c067e86e1fcb2e
Dan Persa <dan.persa@gmail.com>
2011-11-25 01:11:05 +0200

    extracted the machines from specs

--- spec/active_record/active_record_helper.rb
+++ spec/active_record/active_record_helper.rb
@@ -3,6 +3,8 @@ require 'active_record'
 require 'active_support/core_ext/module/aliasing'
 require 'active_record/migrations/create_orders'
 require 'active_record/migrations/create_traffic_lights'
+require 'active_record/samples/traffic_light'
+require 'active_record/samples/order'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
--- spec/mongoid/mongoid_helper.rb
+++ spec/mongoid/mongoid_helper.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 require 'mongoid'
+require 'mongoid/samples/traffic_light'
+require 'mongoid/samples/order'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'mongoid/edge-state-machine'
 
 Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db('edge_state_machine_test')
   config.allow_dynamic_fields = false
 end
-
